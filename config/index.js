a45de4fc6694463f90e5a24464464294fa0fdb1d
Tamás Ujj <Tamas.Ujj@LEGO.com>
2022-03-09 15:04:20 +0100

    env modified

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
     })
   );
 
